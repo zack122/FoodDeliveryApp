64da5c1008a4577b4bb8ea021e0324b048cc48c4
Alex <alexbabaeff42@gmail.com>
2020-12-13 13:24:43 -0800

    Actually demo1 up to additional analysis

--- app/src/main/java/com/example/fooddeliveryapp/Dish.java
+++ app/src/main/java/com/example/fooddeliveryapp/Dish.java
@@ -1,4 +1,61 @@
 package com.example.fooddeliveryapp;
 
+import androidx.annotation.NonNull;
+
+import com.thedeanda.lorem.Lorem;
+import com.thedeanda.lorem.LoremIpsum;
+
 public class Dish {
+
+    // public properties
+  public String name;
+  public String description;
+  public Integer priceInCents;
+  public String imageResourceName;
+
+        //Constructor
+    public Dish(String imageResourceName){
+        this.imageResourceName = imageResourceName;
+
+        populateProperties();
+
+    }
+
+    //public methods
+
+
+    @Override
+    public String toString() {
+        return "Dish{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", princeInCents=" + priceInCents +
+                ", imageResourceName='" + imageResourceName + '\'' +
+                '}';
+    }
+
+    public Boolean equals(Dish dish){
+        return (name.equals(dish.name) &&
+               description.equals(dish.description)&&
+               imageResourceName.equals(dish.imageResourceName)&&
+               priceInCents == dish.priceInCents);
+    }
+
+    //private properties
+    private static Lorem lorem = LoremIpsum.getInstance();
+    private int maxPriceInCents = 1999;
+    private int minPriceInCents = 799;
+
+    //private methods
+    private void populateProperties(){
+        String name = lorem.getTitle(1, 4);
+        String description = lorem.getParagraphs(2, 4);
+
+        this.name = name;
+        this.description = description;
+        this.priceInCents = (int)(Math.random() * (maxPriceInCents = minPriceInCents + 1) + minPriceInCents);
+
+
+    }
+
 }
--- app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
+++ app/src/main/java/com/example/fooddeliveryapp/MainActivity.java
@@ -11,7 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("Hello World");
+        Dish dish1 = new Dish("");
+        System.out.println("dish 1: " + dish1);
+        Dish dish2 = new Dish("");
+        System.out.println("dish 1: " + dish2);
+        Dish dish3 = dish1;
+        System.out.println("dish 3: " + dish3);
 
+
+        System.out.println("dish1 == dish2 " + dish1.equals(dish2));
+        System.out.println("dish1 == dish3 " + dish1.equals(dish3));
+        System.out.println("dish2 == dish3 " + dish2.equals(dish3));
     }
 }
