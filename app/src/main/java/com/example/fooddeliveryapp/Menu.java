e3ca7e7b45d644a9c4e8ed4dc98f58156e2567da
Alex <alexbabaeff42@gmail.com>
2021-01-14 21:51:13 -0800

    fixed my stupidity

--- app/src/main/java/com/example/fooddeliveryapp/Menu.java
+++ app/src/main/java/com/example/fooddeliveryapp/Menu.java
@@ -13,39 +13,44 @@ public class Menu {
         return sharedInstance;
     }
     private static Menu sharedInstance = null;
-    private Menu() {  // Don't allow instantiation outside of this class
+    private Menu() {}  // Don't allow instantiation outside of this class
 
-    }
+    // Public properties
+    public Cuisine selectedDishCuisine;
+    public Integer selectedDishPosition;
 
+    // Public method
     public HashMap<String, ArrayList<Dish>> dishesByCuisine() {
-        if (menu == null) { // lazy instantiation
+        if (menu == null) {
             populateMenu();
         }
-       return menu;
+        return menu;
     }
-    private HashMap<String, ArrayList<Dish>> menu = null;
 
+    // Private property
+    private HashMap<String, ArrayList<Dish>> menu = null;  // "CuisineName" : [ Dish }
+
+    // Private method
     private void populateMenu() {
         menu = new HashMap<String, ArrayList<Dish>>();
 
-                // Iterate through every possible Cuisine type
+        // Iterate through every possible Cuisine type
         for (Cuisine cuisine : Cuisine.values()) {
             ArrayList<Dish> dishesList = new ArrayList<Dish>();
-            // insert 6 dishes for each cuisine
-            for ( int i = 0; i < 6; i++) {
-                Dish dish = new Dish("");
+
+            // Insert 6 dishes for each cuisine
+            for (int i = 0; i < 6; i++) {
+                String imageResourceName = cuisine.name().toLowerCase() + "0" + (i+1);
+                Dish dish = new Dish(imageResourceName);
                 dishesList.add(dish);
             }
 
-            //Here, dishesList will have 6 dishes added to it
-            // so add it to the menu dictionary
-            menu.put(cuisine.toString(), dishesList);
+            // Here, dishesList will have 6 dishes added to it
+            // So add it to the menu dictionary
+            menu.put(cuisine.name(), dishesList);
         }
 
-        //Here, my menu dictioanry should be all populated
-        System.out.println("menu: " + menu);
+        // Here, my menu dictionary should be all populated
+        System.out.println("Menu: " + menu);
     }
-
-    public Cuisine selectedDishCuisine;
-    public int selectedDishPosition;
-} 
+}
